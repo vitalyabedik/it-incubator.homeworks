84197e4986861337b8ae2fefb04641c27d33fa4a
Vitali Bedik <vitalya.bedik@gmail.com>
2023-04-19 20:58:52 +0300

    hw-4: fix solution with ?

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -28,14 +28,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
-        if (onChangeChecked) {
-            onChangeChecked(e.currentTarget.checked)
-        }
-        if (onChange) {
-            onChange(e)
-        }
-
-
+            onChangeChecked?.(e.currentTarget.checked)
+            onChange?.(e)
     }
 
     const finalInputClassName = s.checkbox
