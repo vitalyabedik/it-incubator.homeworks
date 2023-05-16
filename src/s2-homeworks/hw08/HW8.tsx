3fd6bd91e2b983c554b117edc3a582080cfae1dc
Vitali Bedik <vitalya.bedik@gmail.com>
2023-05-16 22:23:46 +0300

    hw-8: finish hw

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -19,12 +19,10 @@ export type UserType = {
 
 const initialPeople: UserType[] = [
     // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    {_id: 0, name: 'Аладинов Антон', age: 16},
+    {_id: 1, name: 'Бориславов Макс', age: 23},
+    {_id: 2, name: 'Тимошин Роман', age: 19},
+    {_id: 3, name: 'Макаров Михаил', age: 54}
 ]
 
 const HW8 = () => {
@@ -85,7 +83,7 @@ const HW8 = () => {
                     <table id={'hw8-users'} className={s.users}>
                         <thead className={s.thead}>
                         <tr>
-                            <td className={s.nameCol}>Name</td>
+                            <td className={s.nameCol}>Full name</td>
                             <td className={s.ageCol}>Age</td>
                         </tr>
                         </thead>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return [...state].sort((a, b) => {
+                return (
+                    action.payload === 'up'
+                        ? a.name.localeCompare(b.name)
+                        : b.name.localeCompare(a.name)
+                )
+            })
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age > action.payload)
         }
         default:
             return state
