571975a22d8950a0fab44a139cbd25f62b767a0a
Vitali Bedik <vitalya.bedik@gmail.com>
2023-06-21 21:43:46 +0300

    hw-13: finish hw with styles

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -43,23 +43,24 @@ const HW13 = () => {
             })
             .catch((e) => {
                 // дописать
-                if (!x) {
-                    setCode('Ошибка 400!')
-                    setImage(error400)
-                    setText('Ты не отправил success в body вообще!')
-                    setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
-                }
-                if (x === undefined) {
-                    setCode('Ошибка 500!')
-                    setImage(error500)
-                    setText('эмитация ошибки на сервере')
-                    setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
-                }
-                if (x === null) {
-                    setCode('Error!')
-                    setImage(errorUnknown)
-                    setText('Network Error')
-                    setInfo('AxiosError')
+                switch (e.response.status) {
+                    case 400:
+                        setCode('Ошибка 400!')
+                        setImage(error400)
+                        setText('Ты не отправил success в body вообще!')
+                        setInfo('ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                        break;
+                    case 500:
+                        setCode('Ошибка 500!')
+                        setImage(error500)
+                        setText('эмитация ошибки на сервере')
+                        setInfo('ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                        break;
+                    default:
+                        setCode('Error!')
+                        setImage(errorUnknown)
+                        setText('Network Error')
+                        setInfo('AxiosError')
                 }
             })
     }
